05648bbf8a40a098afffd3c11c95157377b37706
dmc95 <d-mc95@hotmail.fr>
2023-04-18 21:34:08 +0200

    ajout d'animation bounce

--- src/containers/MainPage.js
+++ src/containers/MainPage.js
@@ -35,27 +35,27 @@ const MainPage = () => {
             {/* <img className='mx-auto py-4' alt='multiSecteurs' src={MultiSecteurs}/> */}
             <div className="flex flex-wrap h-[1250px] lg:h-[600px] xl:w-[1100px] mx-auto my-5 p-10 shadow-lg rounded-md">
               <div className="h-[230px] w-[600px] md:w-[750px] lg:w-[450px] border-5 mt-5 bg-slate-100 opacity-75 mx-auto rounded z-20 shadow-md">
-              <p className=" mt-10 relative bottom-5 left-8 text-zinc-600 text-2xl font-bold" data-aos="zoom-in" data-aos-delay="800" data-aos-duration="500">Trésorerie</p>
+              <p className="mt-10 relative bottom-5 left-8 text-zinc-600 text-2xl font-bold" data-aos="zoom-in" data-aos-delay="800" data-aos-duration="500">Trésorerie</p>
                 <p className=" relative mr-[120px] text-2xl text-zinc-600 font-bold" data-aos="zoom-in" data-aos-delay="1200" data-aos-duration="500">Fournisseurs</p>
-                <p className=" relative mt-[20px] text-blue-900 text-2xl font-bold" data-aos="zoom-in" data-aos-delay="500" data-aos-duration="500">Clients</p>
+                <p className=" relative mt-[20px] text-blue-900 text-2xl font-bold animate-bounce" data-aos="zoom-in" data-aos-delay="500" data-aos-duration="500">Clients</p>
                 <p className=" relative ml-[70px] mt-[30px] text-zinc-600 text-2xl font-bold" data-aos="zoom-in" data-aos-delay="1500" data-aos-duration="500">Financement</p>
               </div>
               <div className="h-[230px] w-[600px] md:w-[750px] lg:w-[450px] border-5 mt-5 bg-slate-100 opacity-75 mx-auto rounded z-20 shadow-md">
               <p className=" mt-10 relative bottom-5 right-10 text-zinc-600 text-2xl font-bold" data-aos="zoom-in" data-aos-delay="900" data-aos-duration="500">Achats</p>
                 <p className=" relative mr-[100px] text-2xl text-zinc-600 font-bold" data-aos="zoom-in" data-aos-delay="1300" data-aos-duration="500">Stocks</p>
-                <p className=" relative mt-[20px] text-blue-900 text-2xl font-bold" data-aos="zoom-in" data-aos-delay="600" data-aos-duration="500">Charges</p>
+                <p className=" relative mt-[20px] text-blue-900 text-2xl font-bold animate-bounce" data-aos="zoom-in" data-aos-delay="600" data-aos-duration="500">Charges</p>
                 <p className=" relative ml-[70px] mt-[30px] text-zinc-600 text-2xl font-bold" data-aos="zoom-in" data-aos-delay="1600" data-aos-duration="500">Délais de Rotation</p>
               </div>
               <div className="h-[230px] w-[600px] md:w-[750px] lg:w-[450px] border-5 mt-5 bg-slate-100 opacity-75 mx-auto rounded z-20 shadow-md">
               <p className=" mt-10 relative bottom-5 left-10 text-zinc-600 text-2xl font-bold" data-aos="zoom-in" data-aos-delay="1000" data-aos-duration="500">CA</p>
                 <p className=" relative mr-[120px] text-2xl text-zinc-600 font-bold" data-aos="zoom-in" data-aos-delay="1400" data-aos-duration="500">Marge</p>
-                <p className=" relative mt-[20px] text-blue-900 text-2xl font-bold" data-aos="zoom-in" data-aos-delay="700" data-aos-duration="500">Résultats</p>
+                <p className=" relative mt-[20px] text-blue-900 text-2xl font-bold animate-bounce" data-aos="zoom-in" data-aos-delay="700" data-aos-duration="500">Résultats</p>
                 <p className=" relative ml-[20px] mt-[30px] xm:ml-[150px] text-zinc-600 text-2xl font-bold" data-aos="zoom-in" data-aos-delay="1700" data-aos-duration="500">Part de marché</p>
               </div>
               <div className="h-[230px] w-[600px] md:w-[750px] lg:w-[450px] border-5 mt-5  bg-slate-100 opacity-75 mx-auto rounded z-20 backdrop-blur/10 shadow-md">
               <p className=" mt-10 relative bottom-5 right-10 text-zinc-600 text-2xl font-bold" data-aos="zoom-in" data-aos-delay="1100" data-aos-duration="500">Frais</p>
                 <p className=" relative ml-[120px] text-2xl text-zinc-600 font-bold" data-aos="zoom-in" data-aos-delay="1500" data-aos-duration="500">Salaires</p>
-                <p className=" relative mt-[20px] text-blue-900 text-2xl font-bold" data-aos="zoom-in" data-aos-delay="800" data-aos-duration="500">Social</p>
+                <p className=" relative mt-[20px] text-blue-900 text-2xl font-bold animate-bounce" data-aos="zoom-in" data-aos-delay="800" data-aos-duration="500">Social</p>
                 <p className=" relative mr-[90px] mt-[30px] text-zinc-600 text-2xl font-bold"  data-aos-delay="1800" data-aos-duration="500">Turn-Over</p>
               </div>
               <img alt="logo" src={logo} className=" collapse xl:visible z-0 w-[100px] h-[100px] absolute lg:ml-[460px] lg:mt-[210px]"/>
