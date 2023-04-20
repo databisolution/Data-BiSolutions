c78b7511f09599e62727202ea81d9f930e1d1224
dmc95 <d-mc95@hotmail.fr>
2023-04-20 20:41:02 +0200

    kpi image

--- src/components/KpiCircle.js
+++ src/components/KpiCircle.js
@@ -1,23 +1,26 @@
 import React from "react";
 import Kpi from "../containers/assets/images/KPI.PNG";
+import Cursor from "./Cursor";
+import '../kpi.css'
 
 
 
-const KpiCircle = () => {
+const KpiCircle = ({mouseOverEvent, mouseOutEvent}) => {
   return (
-    <div className="h-[500px] flex flex-col justify-around z-10 w-[1440px] mx-auto">
-      <div>
-        <p>TEMPS DE PRODUCTIONS / EFFECTIF</p>
+    <div className="h-[500px] flex flex-col justify-around z-0 w-[1440px] mx-auto ">
+      <Cursor />
+      <div onMouseOut={mouseOutEvent} onMouseOver={mouseOverEvent}>
+        <p onMouseOut={mouseOutEvent} onMouseOver={mouseOverEvent}>TEMPS DE PRODUCTIONS / EFFECTIF</p>
       </div>
       
       <div className="flex flex-row justify-between">
 
       <div className="flex flex-col w-[440px] h-[300px] justify-between">
-        <p>Délai de production / article</p>
-        <p>MARGE NETTE / COMMERCIAL</p>
-        <p>TOP 10 Fournisseurs</p>
-        <p>Taux de conversion</p>
-        <p>Coût moyen par embauche</p>
+        <p onMouseOut={mouseOutEvent} onMouseOver={mouseOverEvent}>Délai de production / article</p>
+        <p onMouseOut={mouseOutEvent} onMouseOver={mouseOverEvent}>MARGE NETTE / COMMERCIAL</p>
+        <p onMouseOut={mouseOutEvent} onMouseOver={mouseOverEvent}>TOP 10 Fournisseurs</p>
+        <p onMouseOut={mouseOutEvent} onMouseOver={mouseOverEvent}>Taux de conversion</p>
+        <p onMouseOut={mouseOutEvent} onMouseOver={mouseOverEvent}>Coût moyen par embauche</p>
       </div>
       <div className="z-0">
             <img
@@ -29,17 +32,17 @@ const KpiCircle = () => {
             />
           </div>
       <div className="flex flex-col w-[440px] h-[300px] justify-between">
-        <p>EVOLUTION MENSUELLE DU RESULTAT</p>
-        <p>Ticket moyen / dépense frais</p>
-        <p>MARGE / ARTICLE</p>
-        <p>Taux de rotation</p>
-        <p>Age moyen / poste</p>
+        <p onMouseOut={mouseOutEvent} onMouseOver={mouseOverEvent}>EVOLUTION MENSUELLE DU RESULTAT</p>
+        <p onMouseOut={mouseOutEvent} onMouseOver={mouseOverEvent}>Ticket moyen / dépense frais</p>
+        <p onMouseOut={mouseOutEvent} onMouseOver={mouseOverEvent}>MARGE / ARTICLE</p>
+        <p onMouseOut={mouseOutEvent} onMouseOver={mouseOverEvent}>Taux de rotation</p>
+        <p onMouseOut={mouseOutEvent} onMouseOver={mouseOverEvent}>Age moyen / poste</p>
       </div>
 
       </div>
       <div className="flex justify-center">
-        <p>EVOLUTION DE LA TRESORERIE</p>
-        <p>% répartition des charges</p>
+        <p onMouseOut={mouseOutEvent} onMouseOver={mouseOverEvent}>EVOLUTION DE LA TRESORERIE</p>
+        <p onMouseOut={mouseOutEvent} onMouseOver={mouseOverEvent}>% répartition des charges</p>
       </div>
     </div>
   );
