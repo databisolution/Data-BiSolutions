4d2fe62b12b7a15d00e15ca39cf439e130c575c6
dmc95 <d-mc95@hotmail.fr>
2023-09-28 15:53:03 +0200

    maj pourquoi dbis couleur

--- src/containers/NotreVision.js
+++ src/containers/NotreVision.js
@@ -17,15 +17,15 @@ const NotreVision = () => {
       <section className="h-[3750px]">
         <div>
           <h2 className="my-5 sx:mt-20 font-extrabold text-3xl">
-            Utilisez une Solution dédié pour votre stratégie
+            Utilisez une solution dédié pour votre stratégie
           </h2>
         </div>
         <div className="sx:my-10 w-44 h-1 bg-cyan-500 mx-auto rounded-md"></div>
-        <div className="relative w-full sx:w-[500px] md:w-[600px] lg:w-[800px] 2xl:w-[1000px] h-[1000px] 2xl:h-[1200px] sx:bg-[#d2e0e0] rounded-md sx:mx-auto mt-20 sx:mt-10">
+        <div className="relative w-full sx:w-[500px] md:w-[600px] lg:w-[800px] 2xl:w-[1000px] h-[1000px] 2xl:h-[1200px] bgImg rounded-md sx:mx-auto mt-20 sx:mt-12 max-sx:bg-transparent">
           <div
             className="sx:absolute max-sx:mt-[-40px] max-sx:mx-auto sx:left-[-45px] sx:mt-[130px] w-[290px] slg:w-[300px] md:w-[350px] lg:w-[500px] 2xl:w-[700px] 2xl:h-[420px] h-[320px] bg-slate-900 rounded-md"
             data-aos="fade-right"
-            data-aos-duration="1200"
+            data-aos-duration="1000"
           >
             <img
               className="object-cover w-[290px] slg:w-[300px] md:w-[350px] lg:w-[500px] 2xl:w-[700px] 2xl:h-[420px] h-[320px] rounded-md"
@@ -35,7 +35,7 @@ const NotreVision = () => {
           </div>
           <div
             data-aos="fade-left"
-            data-aos-duration="1200"
+            data-aos-duration="1000"
             className="p-12 max-sx:top-[250px] max-xm:mx-auto max-xm:px-10 text-left w-full absolute sx:right-[-40px] sx:mt-[165px] sx:w-[400px] md:w-[500px] 2xl:h-[350px] 2xl:leading-8  sm:leading-4 leading-4 text-sm 2xl:text-md h-[250px] bg-slate-100 rounded-md shadow-xl"
           >
             <h2 className="font-semibold mb-2 md:mb-5">
@@ -54,7 +54,7 @@ const NotreVision = () => {
           </div>
           <div
             data-aos="fade-left"
-            data-aos-duration="1200"
+            data-aos-duration="1000"
             className="sx:absolute max-sx:mt-60 max-sx:mx-auto sx:right-[-45px] sx:mt-[530px] w-[290px] slg:w-[300px] md:w-[350px] lg:w-[500px] 2xl:w-[700px] 2xl:mt-[630px] 2xl:h-[420px] h-[320px] bg-slate-900 rounded-md"
           >
             <img
@@ -65,7 +65,7 @@ const NotreVision = () => {
           </div>
           <div
             data-aos="fade-right"
-            data-aos-duration="1200"
+            data-aos-duration="1000"
             className="p-12 max-sx:top-[800px] max-xm:mx-auto max-xm:px-10 text-left w-full absolute sx:left-[-40px] sx:mt-[565px] 2xl:mt-[665px] sx:w-[400px] md:w-[500px] 2xl:h-[350px] 2xl:leading-8 sm:leading-4 leading-4 text-sm 2xl:text-md h-[250px] bg-slate-100 rounded-md shadow-xl"
           >
             <h2 className="font-semibold mb-2 md:mb-5">Ergonomique</h2>
@@ -77,15 +77,16 @@ const NotreVision = () => {
             />
           </div>
         </div>
-        <h2 className="mt-20 sx:mt-14 font-extrabold text-2xl text-cyan-500">
+        <h2 className="mt-20 sx:mt-14 font-extrabold text-2xl text-black">
           Pérennisez et sécurisez les flux d'informations au sein de votre
           structure
         </h2>
+        <div className="mt-[24px] sx:mt-10 w-44 h-1 bg-cyan-500 mx-auto rounded-md"></div>
         <div className="relative w-full sx:w-[500px] md:w-[600px] lg:w-[800px] 2xl:w-[1000px] mx-auto">
           <div
             data-aos="fade-right"
-            data-aos-duration="1200"
-            className="sx:absolute max-sx:mt-10 max-sx:mx-auto sx:left-[-45px] sx:mt-[130px] w-[290px] slg:w-[300px] md:w-[350px] lg:w-[500px] 2xl:w-[700px] 2xl:h-[420px] h-[320px] bg-slate-900 rounded-md"
+            data-aos-duration="1000"
+            className="sx:absolute max-sx:mt-10 max-sx:mx-auto sx:left-[-45px] sx:mt-[70px] w-[290px] slg:w-[300px] md:w-[350px] lg:w-[500px] 2xl:w-[700px] 2xl:h-[420px] h-[320px] bg-slate-900 rounded-md"
           >
             <img
               className="object-cover w-[290px] slg:w-[300px] md:w-[350px] lg:w-[500px] 2xl:w-[700px] 2xl:h-[420px] h-[320px] rounded-md"
@@ -96,8 +97,8 @@ const NotreVision = () => {
 
           <div
             data-aos="fade-left"
-            data-aos-duration="1200"
-            className="p-12 max-sx:top-[190px] max-xm:mx-auto max-xm:px-10 text-left w-full absolute sx:right-[-40px] sx:mt-[165px] sx:w-[400px] md:w-[500px] 2xl:h-[350px] 2xl:leading-8 sm:leading-4 leading-4 text-sm 2xl:text-md h-[250px] bg-slate-100 rounded-md shadow-xl"
+            data-aos-duration="1000"
+            className="p-12 max-sx:top-[190px] max-xm:mx-auto max-xm:px-10 text-left w-full absolute sx:right-[-40px] sx:mt-[105px] sx:w-[400px] md:w-[500px] 2xl:h-[350px] 2xl:leading-8 sm:leading-4 leading-4 text-sm 2xl:text-md h-[250px] bg-slate-100 rounded-md shadow-xl"
           >
             <h2 className="font-semibold mb-2 md:mb-5">Automatique</h2>
             <ul className="list-disc">
@@ -113,11 +114,11 @@ const NotreVision = () => {
             </ul>
           </div>
         </div>
-
+              
         <div className="relative w-full sx:w-[500px] md:w-[600px] lg:w-[800px] 2xl:w-[1000px] mx-auto">
           <div
             data-aos="fade-left"
-            data-aos-duration="1200"
+            data-aos-duration="1000"
             className="sx:absolute max-sx:mt-48 max-sx:mx-auto sx:right-[-45px] sx:mt-[630px] w-[290px] slg:w-[300px] md:w-[350px] lg:w-[500px] 2xl:w-[700px] 2xl:h-[420px] h-[320px] bg-slate-900 rounded-md"
           >
             <img
@@ -128,7 +129,7 @@ const NotreVision = () => {
           </div>
           <div
             data-aos="fade-right"
-            data-aos-duration="1200"
+            data-aos-duration="1000"
             className="p-12 max-sx:top-[200px] max-xm:mx-auto max-xm:px-10 text-left w-full absolute sx:left-[-40px] mt-[10px] sx:mt-[665px] sx:w-[400px] md:w-[500px] 2xl:h-[350px] 2xl:leading-8 sm:leading-4 leading-4 text-sm 2xl:text-md h-[250px] bg-slate-100 rounded-md shadow-xl"
           >
             <h2 className="font-semibold mb-2 md:mb-5">Sécurité</h2>
@@ -142,13 +143,14 @@ const NotreVision = () => {
           </div>
         </div>
 
-        <h2 className="relative top-[180px] sx:top-[1080px]  xl:top-[1080px] 2xl:top-[1130px] font-extrabold text-2xl text-cyan-500">
+        <h2 className="relative top-[180px] sx:top-[1080px]  xl:top-[1080px] 2xl:top-[1130px] font-extrabold text-2xl text-black">
           Optimisez le temps de vos équipes et améliorer les process avec des
           applications métiers
         </h2>
+
         <VisionLi
           MainDiv={
-            "mt-[200px] sx:mt-[1070px] relative w-full sx:w-[500px] md:w-[600px] lg:w-[800px] 2xl:w-[1000px] mx-auto"
+            "mt-[240px] sx:mt-[1100px] relative w-full sx:w-[500px] md:w-[600px] lg:w-[800px] 2xl:w-[1000px] mx-auto"
           }
           img={auto}
           title={"Processus automatisés"}
@@ -157,11 +159,11 @@ const NotreVision = () => {
           el3={"Associer les données des applications à vos dashboard"}
           el4={"Créer des editions personnalisées et dédiées"}
         />
-
+         <div className="relative top-[-355px] sx:top-16 2xl:top-[80px] sx:my-10 w-44 h-1 bg-cyan-500 mx-auto rounded-md"></div>
         <div className="relative w-full sx:w-[500px] md:w-[600px] lg:w-[800px] 2xl:w-[1000px] mx-auto ">
           <div
             data-aos="fade-down"
-            data-aos-duration="1200"
+            data-aos-duration="1000"
             className="sx:absolute max-sx:mt-72 max-sx:mx-auto sx:right-[-45px] sx:mt-[630px] w-[290px] slg:w-[300px] md:w-[350px] lg:w-[500px] 2xl:w-[700px] 2xl:h-[420px] h-[320px] bg-slate-900 rounded-md"
           >
             <img
